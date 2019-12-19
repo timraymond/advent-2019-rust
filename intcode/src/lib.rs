779d103a84dc5297130afa9fb4bd3ce34b6c48ae
Tim Raymond <tim@timraymond.com>
2019-12-19 15:44:41 -0500

    WIP: add input / output instructions
    In order to solve Day 5, I needed to add instructions that produce
    output and consume input. Rather than tie those to concrete
    implementations, I've created traits for something of a reader /
    writer. At this point, everything compiles without warnings, but I need
    to update the existing tests so that they can have a test input /
    output buffer.

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -4,82 +4,115 @@ mod tests {
 
     #[test]
     fn test_add() {
-        let mut prg = vec!(1, 4, 5, 6, 1, 2, 0, 0);
-        exec(&mut prg);
-        assert_eq!(prg[6], 3);
+        let mut prg = vec!(1, 5, 6, 7, 99, 1, 2, 0, 0);
+        if let Err(msg) = exec(&mut prg) {
+            panic!(false);
+        } else {
+            assert_eq!(prg[7], 3);
+        }
     }
 
     #[test]
     fn test_mul() {
-        let mut prg = vec!(2, 4, 5, 6, 1, 2, 0, 0);
-        exec(&mut prg);
-        assert_eq!(prg[6], 2);
+        let mut prg = vec!(2, 5, 6, 7, 99, 1, 2, 0, 0);
+        if let Err(msg) = exec(&mut prg) {
+            panic!(false);
+        } else {
+            assert_eq!(prg[7], 2);
+        }
     }
 
     #[test]
-    fn test_halt() {
-        let mut prg = vec!(1, 4, 5, 6, 1, 2, 0, 0, 99, 0, 0, 0, 2, 4, 5, 6);
-        exec(&mut prg);
-        assert_eq!(prg[6], 3);
-    }
-}
-
-const WORD_SIZE: usize = 4;
-
-enum Instruction {
-    Add(usize, usize, usize),
-    Mul(usize, usize, usize),
-    Halt,
-    Invalid,
-}
-
-#[derive(Clone,Copy)]
-enum Condition {
-    Okay,
-    Halted,
-    Exception,
-}
-
-impl Instruction {
-    fn exec(&self, prg: &mut [usize], cond: &Condition) -> Condition {
-        if let Condition::Okay = cond {
-            match self {
-                Instruction::Add(a, b, out) => {
-                    prg[out.clone()] = prg[a.clone()] + prg[b.clone()];
-                    return Condition::Okay;
-                },
-                Instruction::Mul(a, b, out) => {
-                    prg[out.clone()] = prg[a.clone()] * prg[b.clone()];
-                    return Condition::Okay;
-                },
-                Instruction::Halt => Condition::Halted,
-                Instruction::Invalid => Condition::Exception,
+    fn test_input() {
+        let mut prg = vec!(3, 0, 4, 0, 99);
+        let input = vec![42];
+        match exec(&mut prg) {
+            Ok(out) => {
+                assert_eq!(out[0], 42);
+            },
+            Err(msg) => {
+                panic!(msg)
             }
-        } else {
-            return *cond
         }
     }
 }
 
-fn decode(ins: &[usize]) -> Instruction {
-    if ins.len() != WORD_SIZE {
-        return Instruction::Invalid
+enum Instruction {
+    Add(i32, i32, i32),
+    Mul(i32, i32, i32),
+    In(i32),
+    Out(i32),
+    Halt,
+    Invalid,
+}
+
+impl Instruction {
+    fn exec(&self, prg: &mut [i32], input: &impl NumConsumer, output: &impl NumProducer) {
+        match self {
+            Instruction::Add(a, b, out) => {
+                prg[*out as usize] = prg[*a as usize] + prg[*b as usize];
+            },
+            Instruction::Mul(a, b, out) => {
+                prg[*out as usize] = prg[*a as usize] * prg[*b as usize];
+            },
+            Instruction::In(dest) => {
+                prg[*dest as usize] = input.input();
+            }
+            Instruction::Out(source) => {
+                output.output(prg[*source as usize]);
+            }
+            _ => (),
+        }
     }
 
+    fn size(&self) -> usize {
+        match self {
+            Instruction::Add(_, _, _) => 4,
+            Instruction::Mul(_, _, _) => 4,
+            Instruction::In(_) => 2,
+            Instruction::Out(_) => 2,
+            _ => 1,
+        }
+    }
+}
+
+fn decode(ins: &[i32]) -> Instruction {
     match ins[0] {
-        1 => Instruction::Add(ins[1] as usize, ins[2] as usize, ins[3] as usize),
-        2 => Instruction::Mul(ins[1] as usize, ins[2] as usize, ins[3] as usize),
+        1 => Instruction::Add(ins[1], ins[2], ins[3]),
+        2 => Instruction::Mul(ins[1], ins[2], ins[3]),
+        3 => Instruction::In(ins[1]),
+        4 => Instruction::Out(ins[1]),
         99 => Instruction::Halt,
         _ => Instruction::Invalid,
     }
 }
 
-pub fn exec(prg: &mut [usize]) {
-    let mut cond = Condition::Okay;
+pub trait NumProducer {
+    fn output(&self, out: i32);
+}
 
-    for idx in 0..prg.len()/WORD_SIZE {
-        let start = idx*WORD_SIZE;
-        let end = start+WORD_SIZE;
-        cond = decode(&prg[start..end]).exec(&mut prg[..], &cond);
+pub trait NumConsumer {
+    fn input(&self) -> i32;
+}
+
+pub fn exec(prg: &mut [i32], input: &impl NumConsumer, out: &impl NumProducer) {
+    let mut pc = 0;
+
+    loop {
+        println!("pc: {}, slice: {:?}", pc, &prg[pc..]);
+        let ins = decode(&prg[pc..]);
+
+        match ins {
+            Instruction::Halt => {
+                return;
+            },
+            Instruction::Invalid => {
+                panic!("invalid instruction");
+            },
+            _ => {
+                ins.exec(prg, input, out);
+                pc += ins.size();
+            },
+        }
     }
 }
